dce9768c12cda684dbfa694b54c5eda4e781c490
Remi776 <99408740+Remi776@users.noreply.github.com>
2023-03-27 21:51:00 +0300

    ex1 factorial() and triangular()

--- src/seminar_1/Main.java
+++ src/seminar_1/Main.java
@@ -1,6 +1,8 @@
 package seminar_1;
-
+//import seminar_1.task.*;
 import seminar_1.hm.*;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,7 +11,15 @@ public class Main {
 //        int a = task_2.run(arr);
 //        System.out.println(a);
 
-        ex_2.prime_num();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the length of triangle: ");
+        int digit = scanner.nextInt();
+        ex_1.triangular_number(digit);
+        ex_1.factorial(digit);
+
+        //ex_2.prime_num();
+
+
     }
 }
 
--- src/seminar_1/hm/ex_1.java
+++ src/seminar_1/hm/ex_1.java
@@ -1,4 +1,31 @@
 package seminar_1.hm;
 
+import java.util.Scanner;
+
 public class ex_1 {
+    public static void triangular_number(int num) {
+        int count = 0;
+
+        for (int i = 1; i <= num; i++) {
+            for (int j = 1; j <= i; j++) {
+                if (j == i) {
+                    System.out.print(j + " ");
+                    count += j;
+                    break;
+                }
+                System.out.print(j + " + ");
+                count += j;
+            }
+            System.out.printf("= %d\n", count);
+            count = 0;
+        }
+    }
+
+    public static void factorial(int number) {
+        int result = 1;
+        for (int i = 1; i <= number; i++) {
+            result *= i;
+        }
+        System.out.printf("%d! = %d", number, result);
+    }
 }
