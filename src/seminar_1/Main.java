eef036833768f2f7219aec01d145c8198850c490
Remi776 <99408740+Remi776@users.noreply.github.com>
2023-03-27 22:08:40 +0300

    add ex_3.java

--- src/seminar_1/Main.java
+++ src/seminar_1/Main.java
@@ -10,14 +10,16 @@ public class Main {
 //        int[] arr = new int[]{1, 4, 0, 1, 1, 1};
 //        int a = task_2.run(arr);
 //        System.out.println(a);
-
+        System.out.println("Exercise 1");
+        System.out.println("-------------------------------------------------------------------------------------------");
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the length of triangle: ");
         int digit = scanner.nextInt();
         ex_1.triangular_number(digit);
         ex_1.factorial(digit);
-
-        //ex_2.prime_num();
+        System.out.println("\n-------------------------------------------------------------------------------------------");
+        System.out.println("Exercise 2");
+        ex_2.prime_num();
 
 
     }
--- src/seminar_1/hm/ex_2.java
+++ src/seminar_1/hm/ex_2.java
@@ -3,7 +3,8 @@ package seminar_1.hm;
 public class ex_2 {
     public static void prime_num() {
         boolean flag = true;
-        System.out.println(2);
+        int count = 0;
+        System.out.print(" " + 2 + " ");
         for (int i = 3; i < 1000; i = i + 2) {
             for (int j = 2; j < Math.sqrt(i); j++) {
                 if (i % j == 0) {
@@ -11,8 +12,15 @@ public class ex_2 {
                     break;
                 }
             }
-            if (flag) System.out.println(i);
-            else flag = true;
+            // Что-то типо "нормального/читаемого вывода"
+            if (flag) {
+                System.out.print(" " + i + " ");
+                count += 1;
+            } else flag = true;
+            if (count == 12) {
+                System.out.println();
+                count = 0;
+            }
         }
     }
 }
