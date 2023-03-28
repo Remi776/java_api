65753ec7d446cb10654cca8b73c9c521d135b1c7
Remi776 <99408740+Remi776@users.noreply.github.com>
2023-03-28 13:23:14 +0300

    ex_3 done but need to add check for incorrect input

--- src/seminar_1/Main.java
+++ src/seminar_1/Main.java
@@ -1,10 +1,12 @@
 package seminar_1;
 //import seminar_1.task.*;
+
 import seminar_1.hm.*;
 
 import java.util.Scanner;
 
 public class Main {
+    static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
 //        task_1.run();
 //        int[] arr = new int[]{1, 4, 0, 1, 1, 1};
@@ -12,16 +14,28 @@ public class Main {
 //        System.out.println(a);
         System.out.println("Exercise 1");
         System.out.println("-------------------------------------------------------------------------------------------");
-        Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the length of triangle: ");
         int digit = scanner.nextInt();
         ex_1.triangular_number(digit);
         ex_1.factorial(digit);
         System.out.println("\n-------------------------------------------------------------------------------------------");
         System.out.println("Exercise 2");
+        System.out.println("-------------------------------------------------------------------------------------------");
         ex_2.prime_num();
-
-
+        System.out.println("\n-------------------------------------------------------------------------------------------");
+        System.out.println("Exercise 3");
+        System.out.println("-------------------------------------------------------------------------------------------");
+        boolean flag = true;
+        while (flag){
+            int num1 = ex_3.input();
+            int num2 = ex_3.input();
+            char operator = ex_3.getOperator();
+            int result = ex_3.calculate(num1, num2, operator);
+            System.out.println("result: " + result);
+            System.out.print("Close the calculator? 1.Yes 2.No\n");
+            int close_num = scanner.nextInt();
+            if (close_num == 1) flag = false;
+        }
     }
 }
 
--- src/seminar_1/hm/ex_3.java
+++ src/seminar_1/hm/ex_3.java
@@ -1,4 +1,45 @@
 package seminar_1.hm;
 
+import java.util.Scanner;
+
 public class ex_3 {
+    static Scanner scan = new Scanner(System.in);
+
+    public static int input() {
+        System.out.print("Enter a number: ");
+        int number = 0;      // Почему мне не дает объявить просто переменную int number; ? Требует обязательно присвоить значение. Я же присваиваю значение ниже в цикле...
+        if (scan.hasNextInt()) number = scan.nextInt();
+        else System.out.println("Wrong number. Try again.");
+        return number;
+    }
+
+    public static char getOperator() {
+        System.out.print("Enter ONE of the operators ('+', '-', '*', '/'): ");
+        char oper = 0;
+        if (scan.hasNext()) oper = scan.next().charAt(0);
+        else System.out.println("Wrong operator. Try again.");
+        return oper;
+    }
+
+    public static int calculate(int num1, int num2, char operator) {
+        int result = 0;
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+            default:
+                System.out.println("Error! Wrong operator.");
+                result = calculate(num1, num2, getOperator());
+        }
+        return result;
+    }
 }
