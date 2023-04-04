e9c0cff697f74c70578aacffc49c0675c7f23adc
Remi776 <99408740+Remi776@users.noreply.github.com>
2023-04-04 18:16:04 +0300

    add Task4

--- src/seminar3/hw/Task4.java
+++ src/seminar3/hw/Task4.java
@@ -1,4 +1,52 @@
 package seminar3.hw;
 
+
+import java.util.*;
+
+
 public class Task4 {
+
+    //    public static void arrayDifference(Integer[] array1, Integer[] array2) {
+//        listOne = Arrays.asList(array1);
+//        listTwo = Arrays.asList(array2);
+//        listOne.removeAll(listTwo);
+//        System.out.println(listOne);
+//    }
+
+    public static boolean checkArray(int[] arr, int item) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == item) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void arrayDifference(int[] arr1, int[] arr2) {
+
+        List<Integer> diffList = new ArrayList<>();
+
+        for (int i = 0; i < arr1.length; i++) {
+            if (!checkArray(arr2, arr1[i])) {
+                diffList.add(arr1[i]);
+            }
+        }
+
+        for (int i = 0; i < arr2.length; i++) {
+            if (!checkArray(arr1, arr2[i])) {
+                diffList.add(arr2[i]);
+            }
+        }
+        System.out.println(diffList);
+    }
+
+    public static void arrayIntersection(int[] arr1, int[] arr2) {
+        List<Integer> interList = new ArrayList<>();
+        for (int i = 0; i < arr1.length; i++) {
+            if (checkArray(arr2, arr1[i])) {
+                interList.add(arr1[i]);
+            }
+        }
+        System.out.println(interList);
+    }
 }
--- src/seminar3/hw/View.java
+++ src/seminar3/hw/View.java
@@ -5,17 +5,29 @@ import java.util.Arrays;
 
 public class View {
     public static void main(String[] args) {
-//        int len = TaskTEST.getArrayLength();
-//        int[] data = TaskTEST.getRandomArray(len);
-//        System.out.print("Array: ");
-//        System.out.println(Arrays.toString(data));
-//        Task1.sortArray(data);
-//        System.out.print("Sort Array: ");
-//        System.out.println(Arrays.toString(data));
-//        System.out.println("-----------------------------------------------------------------------");
-//        Task2.delEvenNumbersFromList();
-//        System.out.println("-----------------------------------------------------------------------");
-//        Task3.findMinMaxMeanList();
+        int len = TaskTEST.getArrayLength();
+        int[] data = TaskTEST.getRandomArray(len);
+        System.out.print("Array: ");
+        System.out.println(Arrays.toString(data));
+        Task1.sortArray(data);
+        System.out.print("Sort Array: ");
+        System.out.println(Arrays.toString(data));
+        System.out.println("-----------------------------------------------------------------------");
+        Task2.delEvenNumbersFromList();
+        System.out.println("-----------------------------------------------------------------------");
+        Task3.findMinMaxMeanList();
+        System.out.println("\n-----------------------------------------------------------------------");
+        int[] data1 = {1, 3, 5};
+        int[] data2 = {3, 5, 6, 7};
+        System.out.print("array1: ");
+        System.out.println(Arrays.toString(data1));
+        System.out.print("array2: ");
+        System.out.println(Arrays.toString(data2));
+        System.out.print("intersection: ");
+        Task4.arrayIntersection(data1, data2);
+        System.out.print("difference: ");
+        Task4.arrayDifference(data1, data2);
+
     }
 }
 
