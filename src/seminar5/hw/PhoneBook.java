154144915812ec2709fcf97fab2d9588a985de52
Remi776 <99408740+Remi776@users.noreply.github.com>
2023-04-09 22:27:55 +0300

    add view.java

--- src/seminar5/hw/PhoneBook.java
+++ src/seminar5/hw/PhoneBook.java
@@ -9,44 +9,53 @@ import java.util.Scanner;
 public class PhoneBook {
     public static Scanner scanner = new Scanner(System.in);
     private static Map<String, ArrayList<String>> phoneBookMap = new LinkedHashMap<>();
+    private static ArrayList<String> phoneNumberList = new ArrayList<>();
 
     public static void menu() {
-        System.out.print("Phone book:\n1. add contact\t2. print Phone book\n");
-        int userInput = scanner.nextInt();
         boolean flag = true;
-        if (userInput == 1) {
-            inputName();
-            inputNumber();
-            while (flag) {
-                System.out.println("Add another number?\n1. yes\t 2. no\n");
-                int userSecondPhoneInput = scanner.nextInt();
-                if (userSecondPhoneInput == 1) {
-                    inputNumber();
-                } else {
-                    flag = false;
+        while (flag) {
+            System.out.print("Phone book:\n1. add contact\t2. print Phone book\n");
+            int userInput = scanner.nextInt();
+            if (userInput == 1) {
+                System.out.println("Enter the full name: ex.(Иван Иванов)\n");
+                String name = scanner.nextLine();
+                System.out.print("Enter the phone number: ex.(+ 7 555 555 55 55)\n");
+                String phoneNumber = scanner.nextLine();
+                addPhoneNumberInList(phoneNumber);
+                putContactInMap(name, phoneNumberList);
+                while (flag) {
+                    System.out.println("Add another number?\n1. yes\t 2. no\n");
+                    userInput = scanner.nextInt();
+                    if (userInput == 1) {
+                        phoneNumber = scanner.nextLine();
+                        addPhoneNumberInList(phoneNumber);
+                        putContactInMap(name, phoneNumberList);
+                    } else {
+                        flag = false;
+                    }
                 }
+            } else {
+                System.out.println(phoneBookMap);
+                flag = false;
             }
-        } else {
-            System.out.println(phoneBookMap);
         }
     }
 
-    public static void putContactInMap() {
+//    public static String inputName() {
+//        System.out.println("Enter the full name: ex.(Иван Иванов)\n");
+//        return scanner.nextLine();
+//    }
 
+//    public static String inputNumber() {
+//        System.out.print("Enter the phone number: ex.(+ 7 555 555 55 55)\n");
+//        return scanner.nextLine();
+//    }
 
+    public static void addPhoneNumberInList(String number) {
+        phoneNumberList.add(number);
     }
 
-    public static void inputName() {
-        System.out.println("Enter the full name: ex.(Иван Иванов)\n");
-        String nameUser = scanner.nextLine();
-    }
-
-    public static void addPhoneNumberInList(ArrayList<String> phoneNumberList) {
-        phoneNumberList.add(inputNumber());
-    }
-
-    public static void inputNumber() {
-        System.out.print("Enter the phone number: ex.(+ 7 555 555 55 55)");
-        String userNumber = scanner.nextLine();
+    public static void putContactInMap(String name, ArrayList<String> list) {
+        phoneBookMap.put(name, list);
     }
 }
--- src/seminar5/hw/View.java
+++ src/seminar5/hw/View.java
@@ -1,4 +1,7 @@
 package seminar5.hw;
 
 public class View {
+    public static void main(String[] args) {
+        PhoneBook.menu();
+    }
 }
